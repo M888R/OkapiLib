2ccac2bab89961aa2e4471076afa405ebdb3a3b8
cannon <cannonator007@gmail.com>
2018-07-27 14:36:43 -0700

    added ros_lib to includes, so comms work with ROS, using opcontrol

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -7,66 +7,80 @@
  */
 #include "api.h"
 
-#include "okapi/api.hpp"
-#include "test/testRunner.hpp"
-#include "test/tests/impl/allImplTests.hpp"
+//#include "okapi/api.hpp"
+//#include "test/testRunner.hpp"
+//#include "test/tests/impl/allImplTests.hpp"
+
+#include "ros_lib/rosserial_vex_v5/examples/helloworld.h"
 
 void runHeadlessTests() {
-  using namespace okapi;
+  // using namespace okapi;
 
-  runHeadlessUtilTests();
+  // runHeadlessUtilTests();
 
-  test_print_report();
+  // test_print_report();
 }
 
 void opcontrol() {
-  using namespace okapi;
-  pros::Task::delay(100);
+  setup();
+  // while(1) {
+  //   pros::c::delay(200);
+  //   printf("hi\r\n");
+  // }
 
-  runHeadlessTests();
-  return;
-
-  MotorGroup leftMotors({19_mtr, 20_mtr});
-  MotorGroup rightMotors({13_rmtr, 14_rmtr});
-  Motor armMotor = 15_mtr;
-  armMotor.move(10);
-
-  auto chassis =
-    ChassisControllerFactory::create({19, 20}, {-14}, AbstractMotor::gearset::red, {4_in, 11.5_in});
-
-  Controller controller;
-  ControllerButton btn1(E_CONTROLLER_DIGITAL_A);
-  ControllerButton btn2(E_CONTROLLER_DIGITAL_B);
-  ControllerButton btn3(E_CONTROLLER_DIGITAL_Y);
-  ControllerButton btn4(E_CONTROLLER_DIGITAL_X);
-
-  while (true) {
-    chassis.arcade(controller.getAnalog(E_CONTROLLER_ANALOG_LEFT_Y),
-                   controller.getAnalog(E_CONTROLLER_ANALOG_LEFT_X));
-
-    if (btn1.changedToPressed()) {
-      printf("move distance\n");
-      chassis.moveDistance(12_in);
-    }
-
-    if (btn2.changedToPressed()) {
-      printf("turn angle\n");
-      chassis.turnAngle(90_deg);
-    }
-
-    if (btn3.changedToPressed()) {
-      printf("move arm\n");
-      armMotor.moveRelative(-10, 127);
-    }
-
-    if (btn4.changedToPressed()) {
-      printf("autonomous routine\n");
-      for (int i = 0; i < 4; i++) {
-        chassis.moveDistance(12_in);
-        chassis.turnAngle(90_deg);
-      }
-    }
-
-    pros::Task::delay(10);
-  }
+  /* 
+   *
+   * test code
+   * 
+   *
+   * using namespace okapi;
+   *   pros::Task::delay(100);
+   * 
+   *   runHeadlessTests();
+   *   return;
+   * 
+   *   MotorGroup leftMotors({19_mtr, 20_mtr});
+   *   MotorGroup rightMotors({13_rmtr, 14_rmtr});
+   *   Motor armMotor = 15_mtr;
+   *   armMotor.move(10);
+   * 
+   *   auto chassis =
+   *     ChassisControllerFactory::create({19, 20}, {-14}, AbstractMotor::gearset::red, {4_in, 11.5_in});
+   * 
+   *   Controller controller;
+   *   ControllerButton btn1(E_CONTROLLER_DIGITAL_A);
+   *   ControllerButton btn2(E_CONTROLLER_DIGITAL_B);
+   *   ControllerButton btn3(E_CONTROLLER_DIGITAL_Y);
+   *   ControllerButton btn4(E_CONTROLLER_DIGITAL_X);
+   * 
+   *   while (true) {
+   *     chassis.arcade(controller.getAnalog(E_CONTROLLER_ANALOG_LEFT_Y),
+   *                    controller.getAnalog(E_CONTROLLER_ANALOG_LEFT_X));
+   * 
+   *     if (btn1.changedToPressed()) {
+   *       printf("move distance\n");
+   *       chassis.moveDistance(12_in);
+   *     }
+   * 
+   *     if (btn2.changedToPressed()) {
+   *       printf("turn angle\n");
+   *       chassis.turnAngle(90_deg);
+   *     }
+   * 
+   *     if (btn3.changedToPressed()) {
+   *       printf("move arm\n");
+   *       armMotor.moveRelative(-10, 127);
+   *     }
+   * 
+   *     if (btn4.changedToPressed()) {
+   *       printf("autonomous routine\n");
+   *       for (int i = 0; i < 4; i++) {
+   *         chassis.moveDistance(12_in);
+   *         chassis.turnAngle(90_deg);
+   *       }
+   *     }
+   * 
+   *     pros::Task::delay(10);
+   *   }
+  */
 }
